09290b4a1378ffdcf543298cbf4e97e09eb19bba
Mihai Simion <mihai@simion.dev>
2023-01-28 09:01:51 +0200

    added schema coercion

--- src/clj/xo/router.clj
+++ src/clj/xo/router.clj
@@ -1,12 +1,15 @@
 (ns xo.router
   (:require
-    [muuntaja.core :as m]
-    [reitit.ring :as ring]
-    [reitit.ring.middleware.exception :as rrmex]
-    [reitit.ring.middleware.muuntaja :as muuntaja]
-    [reitit.swagger :as swagger]
-    [reitit.swagger-ui :as swagger-ui]
-    [xo.routes :as r]))
+   [muuntaja.core :as m]
+   [reitit.ring :as ring]
+   [reitit.ring.middleware.exception :as rrmex]
+   [reitit.ring.middleware.parameters :as rrmp]
+   [reitit.ring.middleware.muuntaja :as muuntaja]
+   [reitit.ring.coercion :as coercion]
+   [reitit.coercion.schema]
+   [reitit.swagger :as swagger]
+   [reitit.swagger-ui :as swagger-ui]
+   [xo.routes :as r]))
 
 (def swagger-docs
   ["/swagger.json"
@@ -20,25 +23,29 @@
           :handler (swagger/create-swagger-handler)}}])
 
 (def router-config
-  {:data {;; :coercion   reitit.coercion.spec/coercion
+  {:data {:coercion   reitit.coercion.schema/coercion
           :muuntaja m/instance
           :middleware [swagger/swagger-feature
+                       rrmp/parameters-middleware
                        muuntaja/format-negotiate-middleware
                        muuntaja/format-response-middleware
                        rrmex/exception-middleware
-                       muuntaja/format-request-middleware]}})
+                       muuntaja/format-request-middleware
+                       coercion/coerce-exceptions-middleware
+                       coercion/coerce-request-middleware
+                       coercion/coerce-response-middleware]}})
 
 (defn routes
   [env]
   (ring/ring-handler
-    (ring/router
-      [swagger-docs
-       ["/v1"
-        (r/routes env)]]
-      router-config)
-    (ring/routes
-      (swagger-ui/create-swagger-ui-handler {:path "/"})
-      (ring/redirect-trailing-slash-handler)
-      (ring/create-default-handler
-        {:not-found (constantly {:status 404
-                                 :body "Route not found"})}))))
+   (ring/router
+    [swagger-docs
+     ["/v1"
+      (r/routes env)]]
+    router-config)
+   (ring/routes
+    (swagger-ui/create-swagger-ui-handler {:path "/"})
+    (ring/redirect-trailing-slash-handler)
+    (ring/create-default-handler
+     {:not-found (constantly {:status 404
+                              :body "Route not found"})}))))
